05f33d940fdd697ade4676a1c0cfb3fac2cdc6e5
Amer <m1o1h1a1a1@gmail.com>
2026-08-20 02:41:53 +0300

    finished

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -79,5 +79,12 @@ export function configureApp(app: INestApplication): void {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, document, {
+    customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.11.0/swagger-ui-standalone-preset.js',
+    ],
+  });
 }
